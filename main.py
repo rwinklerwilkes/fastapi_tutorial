649081595520fe482b97f51627017d954dbc5fb3
rwinklerwilkes <rich.w.sas@gmail.com>
2023-10-22 15:32:44 -0400

    Populate SQL database using Chinook

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#https://fastapi.tiangolo.com/async/#in-a-hurry describes when to use async vs normal definitions
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str):
+    return {"item_id": item_id}
